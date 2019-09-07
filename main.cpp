4be99426f5f76321865a6238769cd206b3374939
Dizhenin Vladislav <vdizhenin@gmail.com>
2019-09-07 17:40:50 +0300

    Test Canny

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
 using namespace cv;
+using namespace std;
 int main( int argc, char** argv )
 {
  char* imageName = argv[1];
  Mat image;
- image = imread( "/home/dizheninv/magicLassoOpenCV/build/t1.jpg", IMREAD_COLOR );
+ image = imread( "/home/dizheninv/Pictures/675554.jpg", IMREAD_COLOR );
  if( argc != 2 || !image.data )
  {
-   printf( " No image data \n " );
+   cout<< " No image data \n ";
    return -1;
  }
  Mat gray_image;
- cvtColor( image, gray_image, COLOR_BGR2GRAY );
- imwrite( "/home/dizheninv/magicLassoOpenCV/build/t1.jpg", gray_image );
- namedWindow( imageName, WINDOW_AUTOSIZE );
- namedWindow( "Gray image", WINDOW_AUTOSIZE );
- imshow( imageName, image );
- imshow( "Gray image", gray_image );
- waitKey(0);
+ for (int i=0;i<1000;++i){
+   Canny(image, gray_image, 1, 200000 - i * 1000 , 7, true);
+   imwrite( "/home/dizheninv/magicLassoOpenCV/build/t1.jpg", gray_image );
+   namedWindow( imageName, WINDOW_AUTOSIZE);
+   namedWindow( "Gray image", WINDOW_AUTOSIZE);
+   imshow( imageName, image );
+   imshow( "Canny", gray_image);
+   waitKey(0);
+ }
  return 0;
 }
