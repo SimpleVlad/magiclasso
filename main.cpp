eb3f69a02b6c607c0f00c7439e41e67df4044111
Dizhenin Vladislav <vdizhenin@gmail.com>
2019-09-18 16:56:56 +0300

    Rework

--- main.cpp
+++ main.cpp
@@ -3,25 +3,43 @@
 #include <iostream>
 using namespace cv;
 using namespace std;
-int main( int argc, char** argv )
+
+Mat dst, mask;
+int startX = NULL;
+int startY = NULL;
+
+void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 {
- char* imageName = argv[1];
- Mat image;
- image = imread( "/home/dizheninv/Pictures/Rick_and_Morty_wallpaper_desktop_29_30182908.jpg", IMREAD_COLOR );
- if( argc != 2 || !image.data )
- {
-   cout<< " No image data \n ";
-   return -1;
- }
- Mat canny_image;
- for (int i=0;i<1000;++i){
-   Canny(image, canny_image, 1, 200000 - i * 1000 , 7, true);
-   imwrite( "/home/dizheninv/magicLassoOpenCV/build/t1.jpg", canny_image );
-   namedWindow( imageName, WINDOW_AUTOSIZE);
-   namedWindow( "Gray image", WINDOW_AUTOSIZE);
-   imshow( imageName, image );
-   imshow( "Canny", canny_image);
-   waitKey(0);
- }
- return 0;
+
+      if ((event == EVENT_MOUSEMOVE) && (flags & EVENT_FLAG_LBUTTON)){
+        if ((startX == NULL)&&(startY == NULL)){
+           startX=x;
+           startY=y;
+        }
+        line(dst,Point(x,y),Point(startX,startY),Scalar(255,255,255),1);
+        line(mask,Point(x,y),Point(startX,startY),Scalar(255,255,255),1);
+        imshow("DST", dst);
+        imshow("Mask", mask);
+        startX=x;
+        startY=y;
+      }
+}
+ 
+int main(int argc, char** argv)
+{
+    Mat img = imread("test.jpg");
+    mask = Mat(img.rows, img.cols, CV_8UC3, Scalar(0, 0, 0));
+    dst = img;
+    if ( img.empty() ) 
+    { 
+        cout << "Error loading the image" << endl;
+        return -1; 
+    }
+    namedWindow("DST", 1);
+    namedWindow("Mask", 1);
+    imshow("DST", dst);
+    imshow("Mask", mask);
+    setMouseCallback("DST", CallBackFunc, NULL);
+    waitKey(0);
+    return 0;
 }
