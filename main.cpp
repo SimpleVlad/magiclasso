edb8d9b7fafdc950d3c363169781f77557b63124
Dizhenin Vladislav <vdizhenin@gmail.com>
2019-11-14 18:38:09 +0300

    lapzerocros

--- main.cpp
+++ main.cpp
@@ -25,19 +25,43 @@ using namespace std;
         startY=y;
       }
 }*/
+
+int laplassian_zero_crossing(Mat src, Point seed)
+{
+  Mat lap = src.clone();
+  Laplacian( src, lap, 0.5);
+  print(lap);
+   namedWindow("Te", 1);
+    imshow("Te", lap);
+  if (lap.at<uchar>(seed)==0)
+     return 0;
+  else
+     return 1;
+}
+
+float gradient_magnitude()
+{
+  
+} 
  
 int main(int argc, char** argv)
 {
     Point start_point;
-    Mat bin = Mat(90, 90, CV_8UC1, Scalar(0));
-    
-        circle(bin, Point(45, 45), 20, Scalar(255), -1);
-     
-    // print(bin);
+    Mat bin = Mat(10, 10, CV_8UC1, Scalar(200));
 
+    circle(bin, Point(5, 5), 1, Scalar(255), -1);
+    circle(bin, Point(5, 5), 4, Scalar(100), 1);
+    
+
+    Mat dst = bin.clone();
+
+    cout << laplassian_zero_crossing(bin, Point(1, 1))<< endl;
+    
+    namedWindow("Lap",1);    
     namedWindow("Binar", 1);
     imshow("Binar", bin);
-  
+    imshow("Lap", dst);
+
     waitKey(0);
     return 0;
 }
