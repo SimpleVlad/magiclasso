8cfb57c0ff21b64dad5988842cc57b99bae7a0b6
Dizhenin Vladislav <vdizhenin@gmail.com>
2019-12-07 16:55:54 +0300

    Local cost

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+#include <math.h>
 
 using namespace cv;
 using namespace std;
@@ -26,56 +27,71 @@ using namespace std;
       }
 }*/
 
-int laplassian_zero_crossing(Mat src, Point seed)
+Mat laplassian_zero_crossing(const Mat &src)
 {
-  Mat lap = src.clone();
-  Laplacian( src, lap, 0.5);
-  // print(lap);
-
-  if (lap.at<uchar>(seed)==0)
-     return 0;
-  else
-     return 1;
+  Mat lap;
+  Laplacian( src, lap, CV_32F);
+  threshold(lap, lap, 0, 1, THRESH_BINARY);
+  return lap;
 }
 
-float gradient_magnitude(Mat src, Mat clone, Point seed)
+void gradient_magnitude(const Mat &src, Mat &dx, Mat &dy, Mat &mag)
 {
-  Mat tmp = src.clone();
-  // cvtColor(tmp, tmp, COLOR_BGR2GRAY); for colored img
-  Mat dx, dy;
-  Sobel(tmp, dx, CV_32F, 1, 0);
-  Sobel(tmp, dy, CV_32F, 0, 1);
+  double maxVal;
+  double minVal;
+  Point pointMax, pointMin;
 
-  Mat angle, mag;
-  cartToPolar (dx, dy, clone, angle);
-  return clone.at<uchar>(seed);
+  Sobel(src, dx, CV_32F, 1, 0);
+  Sobel(src, dy, CV_32F, 0, 1);
+  magnitude (dx, dy, mag);
+  minMaxLoc(mag, &minVal, &maxVal);
+  mag = 1 - mag / maxVal;
 }
 
-float gradient_direction(Mat src, Point seed, Point second)
+float local_cost(const Point &p, const Point &q, const Mat &laplasian, 
+                const Mat &magnitude, const Mat &dx, const Mat &dy)
 {
-      
+    static const float wlaplasian = 0.43f;
+    static const float wdirection = 0.43f;
+    static const float wmagitude = 0.14f;
+    float dp;
+    float dq;
+
+    if (dy.at<float>(p) * (q.x - p.x) - dx.at<float>(p) * (q.y - p.y) >= 0)
+    {
+        dp = dy.at<float>(p) * (q.x - p.x) - dx.at<float>(p) * (q.y - p.y);
+        dq = dy.at<float>(q) * (q.x - p.x) - dx.at<float>(q) * (q.y - p.y);
+    }
+    else
+    {
+        dp = dy.at<float>(p) * (p.x - q.x) - dx.at<float>(p) * (p.y - q.y);
+        dq = dy.at<float>(q) * (p.x - q.x) - dx.at<float>(q) * (p.y - q.y);
+    }
+    float grad = (1 / cos(dp) + 1/cos(dq)) / M_PI;
+    return wlaplasian * laplasian.at<float>(q) + wdirection * grad + wmagitude * magnitude.at<float>(q);
 }
 
 int main(int argc, char** argv)
 {
-    Point start_point;
-    Mat bin = Mat(100, 100, CV_8UC1, Scalar(200));
-
-    circle(bin, Point(50, 50), 40, Scalar(255), -1);
-    circle(bin, Point(50, 50), 30, Scalar(100), 1);
     
-    Mat dst = bin.clone();
-    Mat clone = bin.clone();
+    Point start_point(331, 48);
+    Point end_point(396, 85);
+    Mat bin = imread("/home/dizheninv/Desktop/test.png", IMREAD_GRAYSCALE);
+    // circle(bin, Point(331, 48), 20, Scalar(0), -1);
+    // circle(bin, Point(396, 85), 20, Scalar(0), -1);
 
-    gradient_magnitude(dst, clone, Point(1,1)); 
-    cout << laplassian_zero_crossing(bin, Point(1, 1))<< endl;
-    
+    Mat laplasian = laplassian_zero_crossing(bin);
+    Mat dx;
+    Mat dy;
+    Mat magnitude;
+    gradient_magnitude(bin, dx, dy, magnitude);
+    cout << local_cost(start_point, end_point, laplasian, magnitude, dx, dy)<<endl;
     namedWindow("Lap",1);    
     namedWindow("Binar", 1);
-    namedWindow("Mag",1);
-    imshow("Binar", bin);
-    imshow("Lap", dst);
-    imshow("Mag", clone);
+    // namedWindow("Mag",1);
+    imshow("Binar", magnitude);
+    // imshow("Lap", dst);
+    // imshow("Mag", mag);
 
     waitKey(0);
     return 0;
