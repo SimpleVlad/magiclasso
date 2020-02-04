31d97d77e7f7adf9674744ba71777f6b0f4dab4a
Dizhenin Vladislav <vdizhenin@gmail.com>
2020-02-04 16:45:00 +0300

    Start

--- main.cpp
+++ main.cpp
@@ -9,21 +9,22 @@
 using namespace cv;
 using namespace std;
 
-/*void CallBackFunc(int event, int x, int y, int flags, void* userdata)
-{
-      if ((event == EVENT_MOUSEMOVE) && (flags & EVENT_FLAG_LBUTTON)){
-        if ((startX == NULL)&&(startY == NULL)){
-           startX=x;
-           startY=y;
-        }
-        line(dst,Point(x,y),Point(startX,startY),Scalar(255,255,255),1);
-        line(mask,Point(x,y),Point(startX,startY),Scalar(255,255,255),1);
-        imshow("DST", dst);
-        imshow("Mask", mask);
-        startX=x;курс 
-        startY=y;
-      }
-}*/
+
+// /*void CallBackFunc(int event, int x, int y, int flags, void* userdata)
+// {
+//       if ((event == EVENT_MOUSEMOVE) && (flags & EVENT_FLAG_LBUTTON)){
+//         if ((startX == NULL)&&(startY == NULL)){
+//            startX=x;
+//            startY=y;
+//         }
+//         line(dst,Point(x,y),Point(startX,startY),Scalar(255,255,255),1);
+//         line(mask,Point(x,y),Point(startX,startY),Scalar(255,255,255),1);
+//         imshow("DST", dst);
+//         imshow("Mask", mask);
+//         startX=x;курс 
+//         startY=y;
+//       }
+// }*/
 
 struct veght_pixel
 {
@@ -31,12 +32,12 @@ struct veght_pixel
   double cost;
 };
 
-/*class Node
-{
-  public:
-    vector<veght_pixel> val; // in progress
-    veght_pixel back;        // to
-}*/
+// /*class Node
+// {
+//   public:
+//     vector<veght_pixel> val; // in progress
+//     veght_pixel back;        // to
+// }*/
 
 void neighbors(const Point &seed, vector <Point> &arr , int max_x, int max_y)
 {
@@ -117,56 +118,115 @@ bool comp( veght_pixel a, veght_pixel b)
 
 int main(int argc, char** argv)
 {
-    Point start_point(331, 48);
-    Point end_point(331, 47);
-    vector <veght_pixel> L;
-    vector <Point> path;
-    veght_pixel P;
-    vector<Point> processed;
+  // --generate strart value--
+  Point start_point(31, 48);
+  Point end_point(31, 47);
+  Mat bin = imread("/home/dizheninv/Desktop/test.png", IMREAD_GRAYSCALE);
+  Mat cost_map(bin.rows, bin.cols, CV_32FC1, cv::Scalar(2147483647));
+  Mat expanded_map(bin.rows, bin.cols, CV_32FC1, cv::Scalar(2147483647));
 
-    Mat bin = imread("/home/dizheninv/Desktop/test.png", IMREAD_GRAYSCALE);
-    Mat laplasian = laplassian_zero_crossing(bin);
-    Mat dx;
-    Mat dy;
-    Mat magnitude;
-    gradient_magnitude(bin, dx, dy, magnitude);
+  vector <veght_pixel> L;
+  vector <Point> path;
+  veght_pixel P;
+  vector<Point> processed;
 
-    P.pixel = start_point;
-    P.cost = 0;
-    // processed.push_back(P.pixel); 
-    L.push_back(P);
+  Mat laplasian = laplassian_zero_crossing(bin);
+  Mat dx;
+  Mat dy;
+  Mat magnitude;
+  gradient_magnitude(bin, dx, dy, magnitude);
+  // --start algorithm--
 
-    while (!L.empty())
-    {
-       vector<Point> arr;
-       sort(L.begin(), L.end(), comp);
-       P = L.back();
-       cout << "F" << endl;
-       L.pop_back();
-       processed.push_back(P.pixel);
-       neighbors( P.pixel, arr, laplasian.rows, laplasian.cols);
-         cout<<arr<<endl;
-       while  (!arr.empty())
-         {
-           Point st;
-           st = arr.back();
-           arr.pop_back();
-           auto neighbor = find(processed.begin(), processed.end(), st);
-           if (neighbor == processed.end())
-            {
-              double total_weight = P.cost + local_cost(P.pixel,st, laplasian, magnitude, dx, dy);  // rework
-              cout << "Total_weight " << total_weight <<endl;
-     
-            }
-           
-         }
-    }
-
-    namedWindow("Lap",1);
-    namedWindow("Binar", 1);
-    namedWindow("Mag",1);
-    imshow("Binar", magnitude);
-    imshow("Mag", laplasian);
-    waitKey(0);
-    return 0;
+  P.pixel = start_point;
+  P.cost = 0;
+  processed.push_back(P.pixel); 
+  L.push_back(P);
+  cost_map.at<float>(start_point) = P.cost; 
+ 
+ 
+ 
+ 
+  // --test block--
+  // circle(cost_map, P.pixel, 10, 0);
+  cout<< cost_map;
+  namedWindow("Cost",1);
+  imshow("Cost", cost_map);
+  waitKey(0);
+  return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int main(int argc, char** argv)
+
+// {
+//     Point start_point(331, 48);
+//     Point end_point(331, 47);
+//     vector <veght_pixel> L;
+//     vector <Point> path;
+//     veght_pixel P;
+//     vector<Point> processed;
+
+//     Mat bin = imread("/home/dizheninv/Desktop/test.png", IMREAD_GRAYSCALE);
+//     Mat laplasian = laplassian_zero_crossing(bin);
+//     Mat dx;
+//     Mat dy;
+//     Mat magnitude;
+//     gradient_magnitude(bin, dx, dy, magnitude);
+
+//     P.pixel = start_point;
+//     P.cost = 0;
+//     // processed.push_back(P.pixel); 
+//     L.push_back(P);
+
+//     while (!L.empty())
+//     {
+//        vector<Point> arr;
+//        sort(L.begin(), L.end(), comp);
+//        P = L.back();
+//        cout << "F" << endl;
+//        L.pop_back();
+//        processed.push_back(P.pixel);
+//        neighbors( P.pixel, arr, laplasian.rows, laplasian.cols);
+//        cout<<arr<<endl;
+//        while  (!arr.empty())
+//          {
+//            Point st;
+//            st = arr.back();
+//            arr.pop_back();
+//            auto neighbor = find(processed.begin(), processed.end(), st);
+//            if (neighbor == processed.end())
+//             {
+//               double total_weight = P.cost + local_cost(P.pixel,st, laplasian, magnitude, dx, dy);  // rework
+//             //cout << "Total_weight " << total_weight <<endl;
+//             }
+           
+//          }
+//     }
+
+//     namedWindow("Lap",1);
+//     namedWindow("Binar", 1);
+//     namedWindow("Mag",1);
+//     imshow("Binar", magnitude);
+//     imshow("Mag", laplasian);
+//     waitKey(0);
+//     return 0;
+// }
