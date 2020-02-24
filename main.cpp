847d74ef2406ee5739f7f2d92b7dc4afdd3e9316
Dizhenin Vladislav <vdizhenin@gmail.com>
2020-02-24 17:32:32 +0300

    V 2.0

--- main.cpp
+++ main.cpp
@@ -1,30 +1,27 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <cmath>
 #include <string>
 #include <vector>
 #include <queue>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
-// using namespace std;
 
 bool g_flag;
-Mat img_draw , img_pre_draw;
+Mat img, img_pre_draw;
 Point start;
 
 Scalar colored_line(255,0,0);
 
-Mat img, zero_crossing, gradient_magnitude, pre, cost, expand, Ix, Iy;
+Mat zero_crossing, gradient_magnitude, Ix, Iy, hit_map_x, hit_map_y;
 
 struct Pix
 {
-    double value;
     Point next_point;
-
-    Pix(){}
+    double value;
 
     bool operator > (const Pix &b) const
     {
@@ -32,36 +29,13 @@ struct Pix
     }
 };
 
-void val_Ix(const Mat& value)
+void grad_mag()
 {
-  for (int i = 0; i < img.rows; i++){
-        for (int j = 0; j < img.cols - 1; j++){
-            Ix.at<double>(i, j) = (value.at<uchar>(i, j + 1) - value.at<uchar>(i, j)) / 255.0;
-        }
-        Ix.at<double>(i, img.cols - 1) = Ix.at<double>(i, img.cols - 2);
-    }
-}
-
-
-void val_Iy(const Mat& value)
-{
-  for (int j = 0; j < img.cols; j++){
-        for (int i = 0; i < img.rows - 1; i++){
-            Iy.at<double>(i, j) = (value.at<uchar>(i + 1, j) - value.at<uchar>(i, j)) / 255.0;
-        }
-        Iy.at<double>(img.rows - 1, j) = Iy.at<double>(img.rows - 2, j);
-    }
-}
-
-Mat grad_mag()
-{
-    Mat G;
     double max_val = 0.0;
-    G.create(img.rows, img.cols, CV_64FC1);
-    magnitude(Iy, Ix, G);
-    minMaxLoc(G, 0, &max_val);
-    G = 1.0 - G/max_val;
-    return G;
+    gradient_magnitude.create(img.rows, img.cols, CV_64FC1);
+    magnitude(Iy, Ix, gradient_magnitude);
+    minMaxLoc(gradient_magnitude, 0, &max_val);
+    gradient_magnitude = 1.0 - gradient_magnitude / max_val;
 }
 
 double local_cost(const Point& p, const Point& q, bool diag)
@@ -71,88 +45,90 @@ double local_cost(const Point& p, const Point& q, bool diag)
     double dp;
     double dq;
 
-    if ((Iy.at<double>(p.y, p.x) * (q.x - p.x) + (-Ix.at<double>(p.y, p.x)) * (q.y - p.y)) >= 0){
-        dp = Iy.at<double>(p.y, p.x) * (q.x - p.x) + (-Ix.at<double>(p.y, p.x)) * (q.y - p.y);
-        dq = (q.x - p.x) * Iy.at<double>(q.y, q.x) + (q.y - p.y) * (-Ix.at<double>(q.y, q.x));
+    if ((Iy.at<double>(p) * (q.x - p.x) - Ix.at<double>(p) * (q.y - p.y)) >= 0)
+    {
+        dp = Iy.at<double>(p) * (q.x - p.x) - Ix.at<double>(p) * (q.y - p.y);
+        dq = Iy.at<double>(q) * (q.x - p.x) - Ix.at<double>(q) * (q.y - p.y);
     }
-
-    else{
-        dp = Iy.at<double>(p.y, p.x) * (p.x - q.x) + (-Ix.at<double>(p.y, p.x)) * (p.y - q.y);
-        dq = (p.x - q.x) * Iy.at<double>(q.y, q.x) + (p.y - q.y) * (-Ix.at<double>(q.y, q.x));
+    else
+    {
+        dp = Iy.at<double>(p) * (p.x - q.x) + (-Ix.at<double>(p)) * (p.y - q.y);
+        dq = Iy.at<double>(q) * (p.x - q.x) + (-Ix.at<double>(q)) * (p.y - q.y);
     }
-    if (!diag){
+    double pi = acos(-1.0);
+    if (!diag)
+    {
         dp /= sqrt(2);
         dq /= sqrt(2);
     }
-    double pi = acos(-1.0);
-    if (diag)
+    else
+    {
         fG /= sqrt(2);
-    return  0.43 * zero_crossing.at<uchar>(q.y, q.x) + 0.43 * (acos(dp) + acos(dq)) / pi + 0.14 * fG;
+    }
+    return  0.43 * zero_crossing.at<uchar>(q) + 0.43 * (acos(dp)
+            + acos(dq)) / pi + 0.14 * fG;
 }
+
 
 
 void find_min_path(const Point& start)
 {
     Mat in_que;
     Mat skip;
-    in_que.create(img.rows, img.cols, CV_8UC1);
-    skip.create(img.rows, img.cols, CV_8UC1);
+    Mat expand;
+    Mat cost;
+    Pix begin;
     int neighbor[2][8] = {{1, 0, -1, 0, 1, 1, -1, -1},
                           {0, 1, 0, -1, 1, -1, 1, -1}};
-
+    cost.create(img.rows, img.cols, CV_64FC1);
+    expand.create(img.rows, img.cols, CV_8UC1);
+    in_que.create(img.rows, img.cols, CV_8UC1);
+    skip.create(img.rows, img.cols, CV_8UC1);
     expand.setTo(0);
-    cost.setTo(INT_MAX);
     in_que.setTo(0);
     skip.setTo(0);
-    cost.at<double>(start.y, start.x) = 0;
-    in_que.at<uchar>(start.y, start.x) = 1;
+    cost.setTo(INT_MAX);
+    cost.at<double>(start) = 0;
+    in_que.at<uchar>(start) = 1;
     std :: priority_queue < Pix, std :: vector<Pix>, std:: greater<Pix> > L;
-    Pix begin;
     begin.value=0;
     begin.next_point = start;
     L.push(begin);
-    while (!L.empty()){
+    while (!L.empty())
+    {
         Pix P = L.top();
         L.pop();
         Point p = P.next_point;
-        in_que.at<uchar>(p.y, p.x) = 0;
-        if (skip.at<uchar>(p.y, p.x) == 1)
-            continue;
-        expand.at<uchar>(p.y, p.x) = 1;
-        for (int i = 0; i < 8; i++){
+        in_que.at<uchar>(p) = 0;
+        if (skip.at<uchar>(p) == 0)
+        {
+        expand.at<uchar>(p) = 1;
+        for (int i = 0; i < 8; i++)
+        {
             int tx = p.x + neighbor[0][i];
             int ty = p.y + neighbor[1][i];
-            if (tx < 0 || tx >= img.cols || ty < 0 || ty >= img.rows)
-                continue;
-            if (expand.at<uchar>(ty, tx) == 1)
+            if (tx < 0 || tx >= img.cols || ty < 0 || ty >= img.rows || expand.at<uchar>(ty, tx) == 1)
                 continue;
             Point q = Point(tx, ty);
-            double tmp;
-            if (i <= 3)
+            double tmp = cost.at<double>(p) + local_cost(p, q, i <= 3);
+            if (in_que.at<uchar>(q) == 1 && tmp < cost.at<double>(q))
             {
-                tmp = cost.at<double>(p.y, p.x) + local_cost(p, q, true);
-            }
-            else
-            {
-                tmp = cost.at<double>(p.y, p.x) + local_cost(p, q, false);
-            }
-            if (in_que.at<uchar>(q.y, q.x) == 1 && tmp < cost.at<double>(q.y, q.x))
-            {
-                skip.at<uchar>(q.y, q.x) = 1;
+                skip.at<uchar>(q) = 1;
                 continue;
             }
 
-            if (in_que.at<uchar>(q.y, q.x) == 0)
+            if (in_que.at<uchar>(q) == 0)
             {
-                cost.at<double>(q.y, q.x) = tmp;
-                pre.at<Vec2i>(q.y, q.x)[0] = p.x;
-                pre.at<Vec2i>(q.y, q.x)[1] = p.y;
-                in_que.at<uchar>(q.y, q.x) = 1;
+                cost.at<double>(q) = tmp;
+                hit_map_x.at<int>(q)= p.x;
+                hit_map_y.at<int>(q) = p.y;
+                in_que.at<uchar>(q) = 1;
                 Pix val;
-                val.value = cost.at<double>(q.y, q.x);
+                val.value = cost.at<double>(q);
                 val.next_point = q;
                 L.push(val);
             }
+        }
         }
     }
 }
@@ -164,63 +140,59 @@ void onMouse(int event, int x, int y, int flags, void *param)
         start = Point(x, y);
         g_flag = true;
         find_min_path(start);
-        img_draw.copyTo(img_pre_draw);
-        imshow("example", img_draw);
+        img.copyTo(img_pre_draw);
+        imshow("example", img);
     }
     else
        if (event == EVENT_MOUSEMOVE && g_flag)
        {
-          img_pre_draw.copyTo(img_draw);
+          img_pre_draw.copyTo(img);
           Point cur = Point(x, y);
           Point tmp;
           while (cur != start)
           {
-              tmp = Point(pre.at<Vec2i>(cur.y, cur.x)[0], pre.at<Vec2i>(cur.y, cur.x)[1]);
-              line(img_draw, cur, tmp, colored_line, 2);
-              if (tmp == start) break;
+              tmp = Point(hit_map_x.at<int>(cur), hit_map_y.at<int>(cur));
+              line(img, cur, tmp, colored_line, 2);
               cur = tmp;
           }
-          imshow("example", img_draw);
+          imshow("example", img);
        }
     else 
        if (event == EVENT_RBUTTONDOWN)
        {
         g_flag = false;
-        img_pre_draw.copyTo(img_draw);
+        img_pre_draw.copyTo(img);
         imshow("example", img_pre_draw);
        }
 }
 
-int main(){
-    Mat grayscale;
-    Mat value;
-    Mat img_canny;
-    std :: cout << "Enter path: ";
-    std :: string path;
-    std :: cin >> path;
-    namedWindow("example");
+const char* keys =
+{
+    "{help h||}{@image |fruits.jpg|input image name}"
+};
 
-    img = imread(path);
-    pre.create(img.rows, img.cols, CV_32SC2);
-    cost.create(img.rows, img.cols, CV_64FC1);
-    expand.create(img.rows, img.cols, CV_8UC1);
-    Ix.create(img.rows, img.cols, CV_64FC1);
-    Iy.create(img.rows, img.cols, CV_64FC1);
+
+int main( int argc, const char** argv )
+{
+    Mat grayscale;
+    Mat img_canny;
+    Mat test_sobel;
+    CommandLineParser parser(argc, argv, keys);
+    std ::  string filename = parser.get<std :: string>(0);
+    img = imread(samples::findFile(filename), IMREAD_COLOR);
+    namedWindow("lasso");
+    hit_map_x.create(img.rows, img.cols, CV_32SC1);
+    hit_map_y.create(img.rows, img.cols, CV_32SC1);
     cvtColor(img, grayscale, COLOR_BGR2GRAY);
-    grayscale.copyTo(value);
-    img.copyTo(img_draw);
-    img.copyTo(img_pre_draw);
-   
     Canny(grayscale, img_canny, 50, 100);
     threshold(img_canny, zero_crossing, 254, 1, THRESH_BINARY_INV);
-    val_Ix(value);
-    val_Iy(value);
-    gradient_magnitude = grad_mag();
-
-    std :: cout << "completed!" << std :: endl;
-
-    setMouseCallback("example", onMouse, 0);
-
-    imshow("example", img);
+    Sobel(grayscale, Ix, CV_64FC1, 1, 0, 1);
+    Sobel(grayscale, Iy, CV_64FC1, 0, 1, 1);
+    Ix = Ix / 255.0;
+    Iy = Iy / 255.0; 
+    grad_mag();
+    img.copyTo(img_pre_draw);
+    setMouseCallback("lasso", onMouse, 0);
+    imshow("lasso", img);
     waitKey(0);
 }
